347ba5a29a634a076220f2b363262a379e26d7b6
Muhammet Mutlu <mumutlu@k1m02s02.42kocaeli.com.tr>
2024-01-18 20:27:51 +0300

    sentaks-ex01

--- CPP_Module_01/ex01/zombieHorde.cpp
+++ CPP_Module_01/ex01/zombieHorde.cpp
@@ -6,13 +6,13 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 19:32:42 by mumutlu           #+#    #+#             */
-/*   Updated: 2024/01/18 19:42:31 by mumutlu          ###   ########.fr       */
+/*   Updated: 2024/01/18 20:07:18 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* zombieHorde( int N, std::string name ) {
+Zombie* zombieHorde(int N, std::string name) {
     Zombie* horde = new Zombie[N];
     for(int i = 0; i < N; i++) {
         new (&horde[i]) Zombie(name);
