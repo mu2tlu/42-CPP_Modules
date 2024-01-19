2f836cd08bd8c651c6e1d2fa9fb3b332961bb799
Muhammet Mutlu <mumutlu@k1m02s02.42kocaeli.com.tr>
2024-01-19 14:29:47 +0300

    Exercise 00: Correction

--- CPP_Module_01/ex00/main.cpp
+++ CPP_Module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:14:01 by mumutlu           #+#    #+#             */
-/*   Updated: 2024/01/18 19:16:52 by mumutlu          ###   ########.fr       */
+/*   Updated: 2024/01/19 14:27:15 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int main(void) {
     Zombie zombie1;
     
-    zombie1.setter("Zombie 1");
+    zombie1.setter("Foo");
     zombie1.announce();
     std::cout << std::endl;
 
-    Zombie* zombie2 = newZombie("Zombie 2");
+    Zombie* zombie2 = newZombie("Foo");
     zombie2->announce();
     std::cout << std::endl;
     delete zombie2;
